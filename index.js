015c3598174c345d5f979735f8505cba12f9d604
mark.sundahl <mark.sundahl@gmail.com>
2018-02-20 20:23:41 -0500

    Add routes.js with sample handler

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("stuff");
+const app = require('express')();
+const MongoClient = require('mongodb').MongoClient;
+
+const db = {};
+
+require('./routes.js')(app, db);
+
+app.listen(8000, () => {
+  console.log('Listening on 8000')
+});
